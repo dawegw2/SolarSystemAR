a1756382125b55f7afe9d3ff79f053a1439db0a7
dawegw2 <dawegwii@gmail.com>
2022-04-05 16:55:25 -0700

    test

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
     button.innerText = '>';
-
-    const button2 = document.getElementById('hidedesc');
-    button2.innerText = 'hide';
-
     setScene();
     changePlanet();
 };
